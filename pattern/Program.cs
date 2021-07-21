0d3e7f554d726e5da5c6f114a5f4d602591ccbb6
Mahesh Dhikonda <mahesh@genems.com>
2021-07-21 20:11:44 +0530

    added comments

--- pattern/Program.cs
+++ pattern/Program.cs
@@ -10,8 +10,11 @@ namespace pattern
             int row = int.Parse(Console.ReadLine());
             Console.Write("Number of symbols : ");
             int symbol = int.Parse(Console.ReadLine());
+
+            //looping number of rows 
             for (int i = 0; i < row; i++)
             {
+                //displaying number of symbols in same row 
                 for (int j = 0; j < symbol; j++)
                 {
                     Console.Write("/\\");
@@ -20,6 +23,8 @@ namespace pattern
                 if (i != row - 1)
                 {
                     Console.Write("|");
+
+                    //looping symbol*2 times to dispaly a space
                     for (int k = 2; k < symbol * 2; k++)
                     {
                         Console.Write(" ");
